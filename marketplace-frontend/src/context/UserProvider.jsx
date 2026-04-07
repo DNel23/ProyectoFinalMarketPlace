22c7c44496d63b86d1090852ece39e719217515b
DNel23 <nelsondonoso@gmail.com>
2026-04-07 00:46:19 -0400

    Update UserProvider.jsx

--- marketplace-frontend/src/context/UserProvider.jsx
+++ marketplace-frontend/src/context/UserProvider.jsx
@@ -2,35 +2,38 @@ import { createContext, useState, useEffect } from "react";
 
 export const UserContext = createContext();
 
+// Definimos la URL del servidor en Render 
+// motor en la nube
+const API_URL = "https://proyectofinalbackend-g120.onrender.com";
+
 const UserProvider = ({ children }) => {
-
   const [user, setUser] = useState(null);
-  
-
   const [products, setProducts] = useState([]);
 
-
+  
   useEffect(() => {
-
-    const initialProducts = [
-      { id: 1, nombre: "Saco de Papas 25kg", precio: 15000, stock: 10, img: "https://cdnx.jumpseller.com/www-agroloar-cl/image/56008798/sites_products-630c944517a85.jpg?1743427213" },
-      { id: 2, nombre: "Miel Orgánica 1lt", precio: 8000, stock: 5, img: "https://cdnx.jumpseller.com/organic-farm/image/53421865/ofc_products_miel_mf_1.jpg?1729372639" }
-    ];
-    setProducts(initialProducts);
+    const getProducts = async () => {
+      try {
+        const response = await fetch(`${API_URL}/productos`);
+        const data = await response.json();
+        setProducts(data); 
+      } catch (error) {
+        console.error("Error al obtener productos desde la nube:", error);
+      }
+    };
+    getProducts();
   }, []);
-
 
   const login = (userData) => {
     setUser(userData); 
   };
-
 
   const logout = () => {
     setUser(null);
   };
 
   return (
-    <UserContext.Provider value={{ user, setUser, login, logout, products }}>
+    <UserContext.Provider value={{ user, setUser, login, logout, products, API_URL }}>
       {children}
     </UserContext.Provider>
   );
